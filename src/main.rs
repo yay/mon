f44b5227d65a9d04f5f8b511a888335de5a13fc2
Vitalii Kravchenko <vitalyx@gmail.com>
2024-01-09 14:00:50 +0000

    Rename to mon

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,9 @@ fn main() -> eframe::Result<()> {
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "mon",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(mon::App::new(cc))),
     )
 }
 
@@ -32,7 +32,7 @@ fn main() {
             .start(
                 "the_canvas_id", // hardcode it
                 web_options,
-                Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+                Box::new(|cc| Box::new(mon::App::new(cc))),
             )
             .await
             .expect("failed to start eframe");
